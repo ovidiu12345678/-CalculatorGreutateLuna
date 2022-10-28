f7dfe94cab6e8a5961a11c43a47d6f4e95d4b9c1
ovidiu12345678 <104549734+ovidiu12345678@users.noreply.github.com>
2022-10-28 14:26:55 +0300

    add
    Tt

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+class Program
+{
+	static void Main(string[] args)
+	
+	
+		{
+		Console.WriteLine("Introduceti greutatea unui om pe luna!");
+		
+		double greutate = double.Parse(Console.ReadLine());
+		
+		if(greutate < 0)
+		{
+			Console.WriteLine("A-ti introdus o greutate invalida. Greutatea nu poate avea valoare negativa! Încercati din nou!");
+			
+		}
+		
+		else if(greutate == 0)
+	  {
+	  	Console.WriteLine("Valoarea greutatii unii om nu poate fi de 0  kg !");
+	  }
+	 
+		else
+		{
+			double campGravitationalLuna = 0.17;
+			
+			double rezultatGreutateOmPeLuna = 0;
+			
+			rezultatGreutateOmPeLuna = greutate * campGravitationalLuna;
+			
+			Console.WriteLine("Greutatea unui om pe luna este de " + rezultatGreutateOmPeLuna + " " + "kg");
+		}
+	
+	}
+}
